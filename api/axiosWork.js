ed6abdb8a93419f9741b07688d1542a3afea5e14
Ambica Khandelwal <72697359+aashi01kh@users.noreply.github.com>
2023-07-27 18:13:33 +0530

    changes

--- api/axiosWork.js
+++ api/axiosWork.js
@@ -6,7 +6,7 @@ function makeAxiosRequest(lol) {
    // console.log(lol);
   return axios.get('https://app.scrapingbee.com/api/v1/store/google', {
     params: {
-      'api_key': '7PF28B13WMYXGMU10G6VRVRU47VTCGM9XJF4L969VM2VHW53XZ5GDSZ9KXAESW738JJUXVSR35N32ZA9',
+      'api_key': APIKEY,
       'search': lol,
       'add_html': true // Enable this parameter to receive the full HTML content
     }
